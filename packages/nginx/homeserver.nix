471ae20bb798073c1dcc7daffee6d6d361392c1c
caem <caem@dirae.org>
2023-06-25 20:27:56 +0200

    Update nginx config

--- packages/nginx/homeserver.nix
+++ packages/nginx/homeserver.nix
@@ -1,12 +1,15 @@
 { ... }:
 
 {
-    services.nginx.enable = true;
-    services.nginx.virtualHosts."192.168.2.69" = {
-        root = "/mnt/mass/Torrents";
-        extraConfig = ''
-            autoindex on;
-        '';
+    services.nginx = {
+        enable = true;
+        user = "media";
+        virtualHosts."192.168.2.69" = {
+            root = "/mnt/mass/Torrents";
+            extraConfig = ''
+                autoindex on;
+            '';
+        };
     };
 
     networking.firewall.allowedTCPPorts = [ 80 ];
