85d690848d8a538dc3c85f90f64ee59935fc8204
caem <caem@dirae.org>
2023-07-06 23:29:43 +0200

    Update deluge config

--- packages/deluge/homeserver.nix
+++ packages/deluge/homeserver.nix
@@ -9,9 +9,17 @@
         authFile = "/mnt/mass/Services/Deluge/auth";
 
         config = {
-            download_location = "/mnt/mass/Torrents";
+            download_location = "/mnt/mass/Torrents/incomplete";
+            move_completed_path = "/mnt/mass/Torrents";
+            move_completed = true;
+            listen_random_port = false;
+            outgoing_interface = "wg0";
+            listen_interface = "wg0";
             allow_remote = true;
             listen_ports = [ 57597 ];
+            max_active_seeding = 0;
+            max_active_downloading = 5;
+            max_active_limite = 0;
         };
     };
 
