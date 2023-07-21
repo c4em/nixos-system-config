b7538f0ca2b3fe1d50eceb62977591b8caf98273
caem <caem@dirae.org>
2023-07-22 01:48:37 +0200

    Update forgejo config

--- packages/forgejo/dirae.nix
+++ packages/forgejo/dirae.nix
@@ -31,10 +31,24 @@ in
                     forgejo-auto,forgejo-light,forgejo-dark,auto,gitea,arc-green
                 '';
                 DEFAULT_THEME = "forgejo-dark";
+                user = {
+                    REPO_PAGING_NUM = 50;
+                };
+                meta = {
+                    AUTHOR = "dirae.org Forgejo instance";
+                    DESCRIPTION = "Forgejo instance hosting git repositories for dirae.org";
+                    KEYWORDS = "go,git,self-hosted,gitea,forgejo,foss,oss,decentrialised,federation"
+                };
             };
 
             repository = {
                 DEFAULT_BRANCH = "master";
+                DISABLE_STARS = true;
+                ENABLE_PUSH_CREATE_USER = true;
+                DEFAULT_REPO_UNITS = ''
+                    repo.code,repo.releases,repo.issues,repo.pulls
+                '';
+                PREFERRED_LICENSES="GPL-3.0-or-later,AGPL-3.0-or-later"
             };
         };
         
